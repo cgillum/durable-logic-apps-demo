4e320c86e5a8f9ac91224fee1e42a3cee84a2abf
Chris Gillum <cgillum@microsoft.com>
2019-09-20 13:54:22 -0700

    Enabled generating project files using --proj

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using LogicApps.Schema;
@@ -41,11 +43,127 @@ namespace LogicApps.TestApp
                 LogicAppsCompiler.Compile("ComposeHttp", doc, writer);
             }
 
-            Console.WriteLine(buffer.ToString());
+            if (args.Any(arg => arg == "--proj"))
+            {
+                bool force = args.Any(arg => arg == "--force");
+                GenerateFunctionProject("ComposeHttp", doc, force);
+            }
+            else
+            {
+                buffer.Clear();
+                using (var writer = new StringWriter(buffer))
+                {
+                    LogicAppsCompiler.Compile("ComposeHttp", doc, writer);
+                }
+
+                Console.WriteLine(buffer.ToString());
+            }
 
             Console.WriteLine();
             Console.WriteLine("Done");
             await Task.Yield();
+        }
+
+        static void GenerateFunctionProject(string workflowName, WorkflowDocument doc, bool force)
+        {
+            string projectDir = Path.Combine(Environment.CurrentDirectory, workflowName);
+            if (Directory.Exists(projectDir))
+            {
+                if (force)
+                {
+                    Directory.Delete(projectDir, recursive: true);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"There is already a directory at {projectDir}. Use --force to override.");
+                    Console.ResetColor();
+                    Environment.Exit(2);
+                    return;
+                }
+            }
+
+            Directory.CreateDirectory(projectDir);
+
+            WriteSourceCode(projectDir, workflowName, doc);
+            WriteProjectFile(projectDir, workflowName);
+            WriteHostJsonFile(projectDir, workflowName);
+            WriteLocalSettingsJsonFile(projectDir);
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Successfully created functions project at " + projectDir);
+            Console.ResetColor();
+        }
+
+        static void WriteSourceCode(string projectDirectory, string workflowName, WorkflowDocument doc)
+        {
+            string outputFilePath = Path.Combine(projectDirectory, workflowName + ".cs");
+            Console.WriteLine($"Generating {outputFilePath}...");
+
+            using (var writer = File.CreateText(outputFilePath))
+            {
+                LogicAppsCompiler.Compile(workflowName, doc, writer);
+            }
+        }
+
+        static void WriteHostJsonFile(string projectDirectory, string workflowName)
+        {
+            string fileName = "host.json";
+            string outputFilePath = Path.Combine(projectDirectory, fileName);
+            Console.WriteLine($"Generating {outputFilePath}...");
+
+            string hostJsonText = LoadResourceText(fileName);
+            hostJsonText = hostJsonText.Replace("%TaskHubName%", $"{workflowName}TaskHub");
+
+            using (StreamWriter writer = File.CreateText(outputFilePath))
+            {
+                writer.Write(hostJsonText);
+            }
+        }
+
+        static void WriteLocalSettingsJsonFile(string projectDirectory)
+        {
+            string fileName = "local.settings.json";
+            string outputFilePath = Path.Combine(projectDirectory, fileName);
+            Console.WriteLine($"Generating {outputFilePath}...");
+
+            string localSettingsText = LoadResourceText(fileName);
+            localSettingsText = localSettingsText.Replace("%StorageConnectionString%", "UseDevelopmentStorage=true");
+
+            using (StreamWriter writer = File.CreateText(outputFilePath))
+            {
+                writer.Write(localSettingsText);
+            }
+        }
+
+        static void WriteProjectFile(string projectDirectory, string workflowName)
+        {
+            string outputFilePath = Path.Combine(projectDirectory, $"Durable{workflowName}.csproj");
+            Console.WriteLine($"Generating {outputFilePath}...");
+
+            string fileName = "ProjectFile.xml";
+            string projectFileText = LoadResourceText(fileName);
+            using (StreamWriter writer = File.CreateText(Path.Combine(projectDirectory, outputFilePath)))
+            {
+                writer.Write(projectFileText);
+            }
+        }
+
+        static string LoadResourceText(string fileName)
+        {
+            // The format is {ProjectDefaultNamespace}.{FolderName}.{FileName}
+            string resourceName = $"LogicApps.Templates." + fileName;
+            Stream resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName);
+            if (resourceStream == null)
+            {
+                throw new ArgumentException($"The specified resource '{fileName}' was not found in the current assembly.");
+            }
+
+            using (StreamReader reader = new StreamReader(resourceStream))
+            {
+                return reader.ReadToEnd();
+            }
         }
     }
 }
