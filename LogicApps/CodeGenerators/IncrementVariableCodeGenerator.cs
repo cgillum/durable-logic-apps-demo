7d411dc89c3f5c4f9c68e1697e15c5cb03680dd8
Chris Gillum <cgillum@microsoft.com>
2019-10-17 18:00:24 -0700

    Support for IncrementVariable action (#12)

---
+++ LogicApps/CodeGenerators/IncrementVariableCodeGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace LogicApps.CodeGenerators
+{
+    class IncrementVariableCodeGenerator : ActionCodeGenerator
+    {
+        public override ActionType ActionType => ActionType.Inline;
+
+        protected override IEnumerable<string> OnGenerateStatements(JToken input, ExpressionContext context)
+        {
+            JObject inputJson = (JObject)input;
+            string variableName = (string)inputJson["name"];
+            if (inputJson.TryGetValue("value", out JToken jsonValue))
+            {
+                yield return $"{variableName} += {jsonValue};";
+            }
+            else
+            {
+                yield return $"{variableName}++;";
+            }
+        }
+    }
+}
